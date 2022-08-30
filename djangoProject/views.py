d1d558bc6eb47f89364905a610b0a5f227bca779
chenhui <945811271@qq.com>
2022-08-30 09:15:23 +0800

    上传代码

--- djangoProject/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from django.template import loader
-
-
-def test_html(request):
-    t = loader.get_template('index.html')
-    html = t.render()
-    return HttpResponse(html)
